f43b1eeaa82bd746c5840f14122ad3a10a1e845b
Aadiyat (Desktop) <aadiyat.ahmad@gmail.com>
2018-10-23 00:35:55 -0400

    Mae a debug tracer stick out of default pawn, point towards where the player is facing. The trace line extends as far as the player's "reach"

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,7 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Grabber.h"
-
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+#define OUT	//Blank macro. Marks return by reference
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -31,5 +34,34 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+	// Get the player viewpoint this tick
+	FVector ViewPointLocation;
+	FRotator ViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+			OUT ViewPointLocation, OUT ViewPointRotation
+		);
+
+	/*UE_LOG(LogTemp, Warning, TEXT("Locaton: %s | Rotation: %s."),
+			*ViewPointLocation.ToString(), *ViewPointRotation.ToString());*/
+
+	// Draw a red trace out in the world to visualise
+	// End point of trace line (must be relative to origin, perform vector calculation
+	// ViewPointRotation.Vector() Gives a vector of length 1 pointing in the direction the
+	// player is pointing.
+
+	FVector LineTraceEnd = ViewPointLocation + ViewPointRotation.Vector()*Reach;
+	DrawDebugLine(
+		GetWorld(),
+		ViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0, 155),
+		false,
+		0.f,
+		0.f,
+		10);
+	// Raycast out to reach distance (private variable to store reach)
+	// Say what we hit
+
 }
 
